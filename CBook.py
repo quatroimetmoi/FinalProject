b015b397b90d2436603c3a29cce50ec432174c88
quatroimetmoi <luutranvanan@gmail.com>
2026-02-10 16:55:05 +0700

    Update CBook.py

--- CBook.py
+++ CBook.py
@@ -8,17 +8,18 @@ class Book:
         self.price = price
         self.quantity = quantity
 
-        def to_dict(self):
-        return {
-            "book_id": self.book_id,
-            "book_name": self.book_name,
-            "author": self.author,
-            "type": self.book_type,
-            "published_year": self.published_year,
-            "price": self.price,
-            "quantity": self.quantity
-        }
+    def to_dict(self):
+    return {
+        "book_id": self.book_id,
+        "book_name": self.book_name,
+        "author": self.author,
+        "type": self.book_type,
+        "published_year": self.published_year,
+        "price": self.price,
+        "quantity": self.quantity
+    }
 
     def __str__(self):
         return(f"Mã sách: {self.book_id}, Tên sách: {self.book_name}, Tác giả: {self.author}")
+
 
